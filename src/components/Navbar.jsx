2d4f590ad4c93a201cba7c59c5289b4ce8699d70
Waqas Raza <waqasraza123@gmail.com>
2022-12-02 17:27:23 +0500

    refactored navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,11 +64,6 @@ const Navbar = () => {
                         <NavbarItem text="Blog" path="/blog"/>
                     </ul>
 
-                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                        <input type="search" className="form-control form-control-dark text-bg-dark"
-                               placeholder="Search..." aria-label="Search" />
-                    </form>
-
                     <div className="text-end">
                         {handleAuthButtons()}
                     </div>
