6deffb7f0af2b7439ab2cfccaef82cb94aaa09db
Waqas <waqas@Waqass-MacBook-Pro.local>
2024-05-16 22:00:12 +0500

    fixed the footer to the bottom of the page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export default class Footer extends Component {
     render() {
-        return(
-            <div className="container">
-                <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        return (
+            <footer className="container-fluid bg-light py-3 fixed-bottom">
+                <div className="d-flex flex-wrap justify-content-between align-items-center border-top">
                     <div className="col-md-4 d-flex align-items-center">
                         <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                             <svg className="bi" width="30" height="24">
@@ -31,8 +31,8 @@ export default class Footer extends Component {
                             </svg>
                         </a></li>
                     </ul>
-                </footer>
-            </div>
+                </div>
+            </footer>
         );
     }
 }
